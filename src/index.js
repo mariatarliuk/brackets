208782fcfa76e9210d87277c3faaf5cb39d184e8
Maria Tarliuk <m.tarliuk@mail.ru>
2020-10-12 19:02:34 +0300

    :

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+   let configArray = [];
+   for(let i=0;i<bracketsConfig.length;i++) {
+      configArray.push(bracketsConfig[i].join(''))
+   }
+   for(let i=0; i<configArray.length;i++) {
+      if(str.replace(configArray[i], '').length!==str.length) {
+         str = str.replace(configArray[i], '');
+         i=-1;
+      }
+   }
+   return str.length==0 ? true : false
+   }
